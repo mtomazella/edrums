046d90e077b3157dc4f8fee357fd50067ca18b0e
Matheus Tomazella <matheus250504@gmail.com>
2023-01-13 19:12:07 -0300

    Hello drum lib approach

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,71 @@
 #include <Arduino.h>
 #include <MIDI.h>
-
-#include "Queue.hpp"
-#include "Drum.hpp"
+#include <hellodrum.h>
 
 #define MIDI_CHANNEL 1
-
 MIDI_CREATE_DEFAULT_INSTANCE();
 
-Queue *queue = new Queue();
+#define readAndSend(drum, config)                               \
+  drum.singlePiezo(config[0], config[1], config[2], config[3]); \
+  if (drum.hit == true)                                         \
+    MIDI.sendNoteOn(config[4], drum.velocity, MIDI_CHANNEL);
 
-Drum snare = Drum("Snare", 23, 15, 40, queue);
-
-void interruptHandler(void *drum)
-{
-  ((Drum *)drum)->onTrigger();
-}
+byte SNARE[7] = {
+    80, // sensitivity
+    10, // threshold
+    20, // scantime
+    20, // masktime
+    38, // note
+    1,  // curve type
+    33  // pin
+};
+HelloDrum snare(SNARE[6]);
+byte HIHAT[7] = {
+    100, // sensitivity
+    10,  // threshold
+    20,  // scantime
+    50,  // masktime
+    42,  // note
+    1,   // curve type
+    32   // pin
+};
+HelloDrum hihat(HIHAT[6]);
+byte RIDE[7] = {
+    80, // sensitivity
+    10, // threshold
+    20, // scantime
+    50, // masktime
+    51, // note
+    1,  // curve type
+    34  // pin
+};
+HelloDrum ride(RIDE[6]);
+byte CRASH[7] = {
+    50, // sensitivity
+    10, // threshold
+    20, // scantime
+    50, // masktime
+    49, // note
+    1,  // curve type
+    35  // pin
+};
+HelloDrum crash(CRASH[6]);
 
 void setup()
 {
   MIDI.begin(MIDI_CHANNEL);
   Serial.begin(115200);
 
-  attachInterruptArg(snare.triggerPin, interruptHandler, &snare, FALLING);
+  snare.setCurve(SNARE[5]);
+  hihat.setCurve(HIHAT[5]);
+  ride.setCurve(RIDE[5]);
+  crash.setCurve(CRASH[5]);
 }
 
 void loop()
 {
-  QueueItem *item = queue->shift();
-  if (item == NULL)
-    return;
-  MIDI.sendNoteOn(item->pitch, item->velocity, MIDI_CHANNEL);
-  MIDI.sendNoteOff(item->pitch, 0, MIDI_CHANNEL);
+  readAndSend(snare, SNARE);
+  readAndSend(hihat, HIHAT);
+  readAndSend(crash, CRASH);
+  // readAndSend(ride, RIDE);
 }
