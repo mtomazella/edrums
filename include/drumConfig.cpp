2a682a86c94a1eda3e9cb1c15ae9ae5fded9d657
matheustomazella <matheus250504@gmail.com>
2023-07-22 12:33:24 -0300

    New config

--- include/drumConfig.cpp
+++ include/drumConfig.cpp
@@ -20,7 +20,7 @@ byte HIHAT_OPEN[7] = {
     70, // masktime
     46, // note
     2,  // curve type
-    33  // pin
+    26  // pin
 };
 byte HIHAT_CLOSED[7] = {
     60, // sensitivity
@@ -29,7 +29,7 @@ byte HIHAT_CLOSED[7] = {
     50, // masktime
     42, // note
     2,  // curve type
-    33  // pin
+    26  // pin
 };
 HelloDrum hihat(HIHAT_OPEN[6]);
 
@@ -45,13 +45,13 @@ byte RIDE[7] = {
 HelloDrum ride(RIDE[6]);
 
 byte CRASH[7] = {
-    70, // sensitivity
+    60, // sensitivity
     10, // threshold
     20, // scantime
     50, // masktime
-    39, // note
+    49, // note
     1,  // curve type
-    26  // pin
+    14  // pin
 };
 HelloDrum crash(CRASH[6]);
 
@@ -62,7 +62,7 @@ byte BASS[7] = {
     20, // masktime
     36, // note
     0,  // curve type
-    22  // pin
+    23  // pin
 };
 Pedal bass(BASS[6]);
 
@@ -84,6 +84,6 @@ byte HH_PEDAL[7] = {
     50, // masktime
     44, // note
     0,  // curve type
-    21  // pin
+    18  // pin
 };
 Pedal hh_pedal(HH_PEDAL[6]);
